15cad2cb512edc25510e536f4c13bf3196929536
zkrzhangdb <1921728836@qq.com>
2020-06-07 22:57:46 +0800

    topic +direct

---
+++ src/main/java/topic/consumer/Consumer.java
@@ -0,0 +1,41 @@
+package topic.consumer;
+
+import com.rabbitmq.client.*;
+import util.ConnectionUtil;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class Consumer {
+      public  static  void main(String[] args) throws IOException, TimeoutException {
+
+
+
+
+          Connection connection = ConnectionUtil.getConnetion();
+          Channel channel =connection.createChannel();
+          //监听 生产者
+          DefaultConsumer defaultCallback = new DefaultConsumer(channel){
+              @Override
+              //envelope参数，消息的关键信息 消息的路由键 消息的唯一标识等信息
+              //收到消息后的一些处理逻辑
+              public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+                  System.out.println(new String(body,"UTF-8"));
+
+//                  System.out.println(1/0);
+
+                  System.out.println("消费成功。。。。");
+                  // 设置消息确认   参数一 消息的唯一标识 参数2 是否进行批量的确认
+
+                  channel.basicAck(envelope.getDeliveryTag(),false);
+
+              }
+          };
+          channel.basicConsume(ConnectionUtil.QUEUE_NAME,false,defaultCallback);
+
+
+
+
+         }
+
+}
--- src/main/java/topic/consumer/Provider.java
+++ src/main/java/topic/consumer/Provider.java
@@ -1,4 +1,4 @@
-package provider;
+package topic.consumer;
 
 import com.rabbitmq.client.BuiltinExchangeType;
 import com.rabbitmq.client.Channel;
@@ -13,16 +13,18 @@ public class Provider {
       public  static  void main(String[] args) throws Exception, TimeoutException {
           Connection connection = ConnectionUtil.getConnetion();
           Channel channel  = connection.createChannel();
+          channel.exchangeDelete("exchangeTest");
           //定义交换机
-          channel.exchangeDeclare("exchangeTest", BuiltinExchangeType.FANOUT);
+          channel.exchangeDeclare("exchangeTest", BuiltinExchangeType.TOPIC);
           //交换机与队列的绑定
-          channel.queueBind("testQueue1","exchangeTest","");
-          channel.queueBind("testQueue2","exchangeTest","");
-
+          channel.queueBind("testQueue1","exchangeTest","user.*");
+          channel.queueBind("testQueue2","exchangeTest","*.info");
+          channel.queueBind("testQueue3","exchangeTest","#");
 
 //          channel.queueDeclare(ConnectionUtil.QUEUE_NAME,true,false,false,null);
           //消息发送到交换机当中  s1 参数是路由键
-          channel.basicPublish("exchangeTest","",null,"hello".getBytes());
+          //testQueue2收不到任何消息
+          channel.basicPublish("exchangeTest","user.ss",null,"hello".getBytes());
           channel.close();
           connection.close();
       }
